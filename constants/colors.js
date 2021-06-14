2cdae0034d57fead36cd7ba97718db895958da81
mg143pavankumar <pavansharma.mg143@gmail.com>
2021-06-14 14:41:59 +0530

    added button components

--- constants/colors.js
+++ constants/colors.js
@@ -1,5 +1,5 @@
 export const primaryblue = "#022F47";
-export const secondaryblue = "##0EA5E9";
+export const secondaryblue = "#0EA5E9";
 export const secondarylightblue = "#14CDDB";
 export const primarytextcolor = "#707070";
 export const secondarytextcolor = "#A5B4BC";
--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,14 @@
-import Image from "next/image";
+import Button from "./../components/Button/Button";
+import { useRouter } from "next/router";
 
 const PageNotFound = () => {
+  const router = useRouter();
   return (
     <div className="flex flex-col justify-center items-center h-screen">
       <img src="images/404Error.gif" />
+      <Button onClick={() => router.replace("/")} width="300">
+        Back To Home
+      </Button>
     </div>
   );
 };
