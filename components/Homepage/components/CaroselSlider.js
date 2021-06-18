80f2dc8994546350e3734f0da4473d745d0d0956
mg143pavankumar <pavansharma.mg143@gmail.com>
2021-06-18 15:57:26 +0530

    Added our top project section

---
+++ components/Homepage/components/CaroselSlider.js
@@ -0,0 +1,65 @@
+import Slider from "react-slick";
+import { topProjectsData } from "../../../data/projectsData/topProjectsData";
+import ProjectDemoCard from "./ProjectDemoCard";
+
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const CaroselSlider = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <div className="px-11 md:px-24 bg-gray-50">
+      <Slider {...settings}>
+        {topProjectsData.map((item, index) => {
+          return (
+            <ProjectDemoCard
+              key={index}
+              source={item.projectImg}
+              title={item.projectName}
+              description={item.description}
+            />
+          );
+        })}
+      </Slider>
+    </div>
+  );
+};
+
+export default CaroselSlider;
