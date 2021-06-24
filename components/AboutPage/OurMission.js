deea10a8c7ef1fb99726866474771a9b6f54c85a
mg143pavankumar <pavansharma.mg143@gmail.com>
2021-06-24 17:34:43 +0530

    finished aboutUs page

--- components/AboutPage/OurMission.js
+++ components/AboutPage/OurMission.js
@@ -1,12 +1,14 @@
 import MultiCardTemplate from "../MultiCardTemplate";
 import { ourMissionData } from "./../../data/aboutUsData/aboutUsData";
+import Fade from "react-reveal/Fade";
 
 const OurMission = () => {
   return (
     <div className="w-screen flex justify-center items-center bg-primaryblue px-6 py-24 md:px-24">
       <div>
         <MultiCardTemplate>
-        <div className="w-full col-span-2">
+          <Fade left>
+            <div className="w-full col-span-2">
               <h1 className="text-primaryblue font-bold text-2xl">
                 Our Mission
               </h1>
@@ -16,6 +18,8 @@ const OurMission = () => {
                 );
               })}
             </div>
+          </Fade>
+          <Fade right>
             <div className="w-full h-full col-span-2 lg:col-span-1 flex items-center justify-center">
               <img
                 className="w-9/12 md:w-8/12"
@@ -23,6 +27,7 @@ const OurMission = () => {
                 alt="our Mission"
               />
             </div>
+          </Fade>
         </MultiCardTemplate>
       </div>
     </div>
