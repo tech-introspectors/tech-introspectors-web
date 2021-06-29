4d5330d8fffa42fdfdb6c09a1781b909c37aea15
Sai kamal <65682028+Dantusaikamal@users.noreply.github.com>
2021-06-29 21:29:24 +0530

    Update topProjectsData.js

--- data/projectsData/topProjectsData.js
+++ data/projectsData/topProjectsData.js
@@ -1,34 +1,53 @@
 export const topProjectsData = [
   {
     id: 1,
-    projectImg: "images/projectImg/topProjectImg/basic-banking-system.png",
+    projectImg: "images/projectsImg/topProjectImg/basic-banking-system.png",
     projectName: "Basic Banking System",
-    description: "Transection web created using nextjs and mongoodb",
+    description: "Banking web application for transactions created using Nextjs and Mongodb",
   },
   {
     id: 2,
-    projectImg: "images/projectImg/topProjectImg/whatsapp-clone.png",
+    projectImg: "images/projectsImg/topProjectImg/whatsapp-clone.png ",
     projectName: "WhatsApp Clone",
-    description: "We hava built a clone",
+    description: "We have built a WhatsApp clone using NextJS where users can text each other.",
   },
   {
     id: 3,
-    projectImg: "images/404Error.gif",
-    projectName: "Title",
+    projectImg: "images/projectsImg/topProjectImg/basic-banking-system.png",
+    projectName: "Basic Banking App ",
     description:
-      "Here description should be written which must explain about project",
+      "Banking mobile application for transactions created Flutter and SQLite.",
   },
   {
     id: 4,
-    projectImg: "images/404Error.gif",
-    projectName: "Title",
+    projectImg: "images/projectsImg/topProjectImg/obj classification.PNG",
+    projectName: "Object detection using TensorFlow.js",
     description:
-      "Here description should be written which must explain about project",
+      "Object detection model developed using Tensorflow.js capable of detecting over 80 objects. ",
   },
   {
     id: 5,
-    projectImg: "images/404Error.gif",
-    projectName: "Title",
-    description: "Last container",
+    projectImg: "images/projectsImg/topProjectImg/facial recogition.png",
+    projectName: "Facial recognition using NLP",
+    description:
+      "Facial recognition model built using Natural Language Processing.",
+  },
+  {
+    id: 6,
+    projectImg: "images/projectsImg/topProjectImg/hulo-clone.png",
+    projectName: "Hulu clone",
+    description: "A Clone of HULU, a streaming service that is built using ReactJS and IMDB api",
+  },
+    {
+    id: 7,
+    projectImg: "images/projectsImg/topProjectImg/emergency-supplies.PNG",
+    projectName: "COVID Emergency Supplies",
+    description: "A simple website built to collect and display information of Covid-19 resources.",
+  },
+    {
+    id: 8,
+    projectImg: "images/projectsImg/topProjectImg/basic-banking-system.png",
+    projectName: "Music player",
+    description: "A music player built using Flutter that can play music.",
   },
 ];
