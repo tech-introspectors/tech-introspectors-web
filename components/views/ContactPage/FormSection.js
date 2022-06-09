74c2426f2361f784c8954ce1a934cb797c00c0af
Sai kamal <65682028+Dantusaikamal@users.noreply.github.com>
2022-06-09 23:08:42 +0530

    Add files via upload

--- components/views/ContactPage/FormSection.js
+++ components/views/ContactPage/FormSection.js
@@ -1,25 +1,25 @@
-// import Form from "./components/Form";
-// import Fade from "react-reveal";
+import Form from "./components/Form";
+import Fade from "react-reveal";
 
-// const FormSection = () => {
-//   return (
-//     <div className="h-auto w-screen grid grid-cols-2 md:grid-cols-3 md:flex-row justify-center items-center bg-primaryblue px-6 py-24 md:px-11">
-//       <Fade bottom>
-//         <div className="w-full col-span-2 relative flex items-center">
-//           <img
-//             className="lg:absolute max-w-5xl w-full lg:p-11"
-//             src="images/contactUsImg/recevingMail.svg"
-//           />
-//         </div>
-//       </Fade>
+const FormSection = () => {
+  return (
+    <div className="h-auto w-screen grid grid-cols-2 md:grid-cols-3 md:flex-row justify-center items-center bg-primaryblue px-6 py-24 md:px-11">
+      <Fade bottom>
+        <div className="w-full col-span-2 relative flex items-center">
+          <img
+            className="lg:absolute max-w-5xl w-full lg:p-11"
+            src="images/contactUsImg/recevingMail.svg"
+          />
+        </div>
+      </Fade>
 
-//       <div className="col-span-2 md:col-span-1">
-//         <Fade top>
-//           <Form />
-//         </Fade>
-//       </div>
-//     </div>
-//   );
-// };
+      <div className="col-span-2 md:col-span-1">
+        <Fade top>
+          <Form />
+        </Fade>
+      </div>
+    </div>
+  );
+};
 
-// export default FormSection;
+export default FormSection;
