3f2e087cd3a8f8635cf509588617abc703a9bc55
Sai kamal <65682028+Dantusaikamal@users.noreply.github.com>
2022-06-10 01:30:18 +0530

    Add files via upload

--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,16 @@
 const TechBlog = () => {
   return (
+    // <div class="h-screen">
     <a href="" >Temp</a>
+    // </div>
+    // // <html>    
+    // // <script>function redirect(){
+    // //   window.location = "../public/tech-blog/index.html"
+    // // }
+      
+    // // <body onload="redirect()"></body>
+    // // </script>
+    // // </html>
 
   );
 };
